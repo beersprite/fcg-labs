40c147e3287c2cd5e355f6f21d0f9c91c78e977e
yasmin beer <55171112+beersprite@users.noreply.github.com>
2023-02-14 16:24:50 -0300

    lab04tarefa1 done

---
+++ src/shader_fragment-tarefa1.glsl
@@ -0,0 +1,131 @@
+#version 330 core
+
+// Atributos de fragmentos recebidos como entrada ("in") pelo Fragment Shader.
+// Neste exemplo, este atributo foi gerado pelo rasterizador como a
+// interpola��o da posi��o global e a normal de cada v�rtice, definidas em
+// "shader_vertex.glsl" e "main.cpp".
+in vec4 position_world;
+in vec4 normal;
+
+// Matrizes computadas no c�digo C++ e enviadas para a GPU
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+// Identificador que define qual objeto est� sendo desenhado no momento
+#define SPHERE 0
+#define BUNNY  1
+#define PLANE  2
+uniform int object_id;
+
+// O valor de sa�da ("out") de um Fragment Shader � a cor final do fragmento.
+out vec4 color;
+
+void main()
+{
+    // Obtemos a posi��o da c�mera utilizando a inversa da matriz que define o
+    // sistema de coordenadas da c�mera.
+    vec4 origin = vec4(0.0, 0.0, 0.0, 1.0);
+    vec4 camera_position = inverse(view) * origin;
+
+    // O fragmento atual � coberto por um ponto que percente � superf�cie de um
+    // dos objetos virtuais da cena. Este ponto, p, possui uma posi��o no
+    // sistema de coordenadas global (World coordinates). Esta posi��o � obtida
+    // atrav�s da interpola��o, feita pelo rasterizador, da posi��o de cada
+    // v�rtice.
+    vec4 p = position_world;
+
+    // Normal do fragmento atual, interpolada pelo rasterizador a partir das
+    // normais de cada v�rtice.
+    vec4 n = normalize(normal);
+
+    // Vetor que define o sentido da fonte de luz em rela��o ao ponto atual.
+    vec4 l = normalize(vec4(1.0,1.0,0.5,0.0));
+
+    // Vetor que define o sentido da c�mera em rela��o ao ponto atual.
+    vec4 v = normalize(camera_position - p);
+
+    // Vetor que define o sentido da reflex�o especular ideal.
+    //vec4 r = vec4(1.0,0.0,0.0,0.0); // PREENCHA AQUI o vetor de reflex�o especular ideal
+    vec4 r = vec4(-l.x + 2*n.x * (dot(n,l)),-l.y + 2*n.y * (dot(n,l)),-l.z + 2*n.z * (dot(n,l)), -l.w + 2*n.w * (dot(n,l))); // PREENCHA AQUI o vetor de reflex�o especular ideal
+
+    // Par�metros que definem as propriedades espectrais da superf�cie
+    vec3 Kd; // Reflet�ncia difusa
+    vec3 Ks; // Reflet�ncia especular
+    vec3 Ka; // Reflet�ncia ambiente
+    float q; // Expoente especular para o modelo de ilumina��o de Phong
+
+    if ( object_id == SPHERE )
+    {
+        // PREENCHA AQUI
+        // Propriedades espectrais da esfera
+        Kd = vec3(0.8,0.4,0.08);
+        Ks = vec3(0.0,0.0,0.0);
+        Ka = vec3(0.4,0.2,0.04);
+        q = 1.0;
+    }
+    else if ( object_id == BUNNY )
+    {
+        // PREENCHA AQUI
+        // Propriedades espectrais do coelho
+        Kd = vec3(0.08,0.4,0.8); // Reflet�ncia difusa/da superf�cie
+        Ks = vec3(0.8,0.8,0.8); // Reflet�ncia especular
+        Ka = vec3(0.04,0.2,0.4); // Reflet�ncia ambiente
+        q = 32.0; // Expoente especular para o modelo de ilumina��o de Phong
+    }
+    else if ( object_id == PLANE )
+    {
+        // PREENCHA AQUI
+        // Propriedades espectrais do plano
+        Kd = vec3(0.2,0.2,0.2);
+        Ks = vec3(0.3,0.3,0.3);
+        Ka = vec3(0.0,0.0,0.0);
+        q = 20.0;
+    }
+    else // Objeto desconhecido = preto
+    {
+        Kd = vec3(0.0,0.0,0.0);
+        Ks = vec3(0.0,0.0,0.0);
+        Ka = vec3(0.0,0.0,0.0);
+        q = 1.0;
+    }
+
+    // Espectro da fonte de ilumina��o
+    vec3 I = vec3(1.0,1.0,1.0); // PREENCHA AQUI o espectro da fonte de luz
+
+    // Espectro da luz ambiente
+    vec3 Ia = vec3(0.2,0.2,0.2); // PREENCHA AQUI o espectro da luz ambiente
+
+    float lambert = max(0, dot(n, l));
+    // Termo difuso utilizando a lei dos cossenos de Lambert
+    vec3 lambert_diffuse_term = vec3(Kd.x * I.x * lambert, Kd.y * I.y * lambert, Kd.z * I.z * lambert); // PREENCHA AQUI o termo difuso de Lambert
+
+    // Termo ambiente
+    vec3 ambient_term = vec3(Ka.x * Ia.x, Ka.y * Ia.y, Ka.z * Ia.z); // PREENCHA AQUI o termo ambiente
+
+    float specular_term = pow(max(0, dot(r, v)), q);
+    // Termo especular utilizando o modelo de ilumina��o de Phong
+    vec3 phong_specular_term  = vec3(Ks.x * I.x * specular_term, Ks.y * I.y * specular_term, Ks.z * I.z * specular_term); // PREENCH AQUI o termo especular de Phong
+
+    // NOTE: Se voc� quiser fazer o rendering de objetos transparentes, �
+    // necess�rio:
+    // 1) Habilitar a opera��o de "blending" de OpenGL logo antes de realizar o
+    //    desenho dos objetos transparentes, com os comandos abaixo no c�digo C++:
+    //      glEnable(GL_BLEND);
+    //      glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    // 2) Realizar o desenho de todos objetos transparentes *ap�s* ter desenhado
+    //    todos os objetos opacos; e
+    // 3) Realizar o desenho de objetos transparentes ordenados de acordo com
+    //    suas dist�ncias para a c�mera (desenhando primeiro objetos
+    //    transparentes que est�o mais longe da c�mera).
+    // Alpha default = 1 = 100% opaco = 0% transparente
+    color.a = 1;
+
+    // Cor final do fragmento calculada com uma combina��o dos termos difuso,
+    // especular, e ambiente. Veja slide 129 do documento Aula_17_e_18_Modelos_de_Iluminacao.pdf.
+    color.rgb = lambert_diffuse_term + ambient_term + phong_specular_term;
+
+    // Cor final com corre��o gamma, considerando monitor sRGB.
+    // Veja https://en.wikipedia.org/w/index.php?title=Gamma_correction&oldid=751281772#Windows.2C_Mac.2C_sRGB_and_TV.2Fvideo_standard_gammas
+    color.rgb = pow(color.rgb, vec3(1.0,1.0,1.0)/2.2);
+}
